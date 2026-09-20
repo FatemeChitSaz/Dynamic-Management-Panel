424b02f00b221266f66b59c5941a5ee926f13ce3
Fatemeh Chit Saz <fatemeh.chitsaaz7@gmail.com>
2026-09-20 13:43:26 +0330

    Used useReducer instead of useState in getData.js module

--- src/hooks/getData.js
+++ src/hooks/getData.js
@@ -1,24 +1,65 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+import { useEffect, useReducer } from "react";
+
+
+// loading is true because of the logic of first loading. when the first loading will happen then the loading state should be true
+const initialState = {data: null, loading: true, error: null};
+
+const fetchReducer = (state, action) => {
+    switch (action.type) {
+        case "FETCH_START":
+           return {...state, loading: true, error: null };
+        case "FETCH_SUCCESFUL":
+            return {data: action.payload, loading: false, error: null};
+        case "FETCH_ERROR":
+            return {...state, loading: false, error: action.payload};
+        default:
+            return state;
+    }
+}
+
+
 
 export const useGetData = (fetchFn) => {
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+    // const [data, setData] = useState(null);
+    // const [loading, setLoading] = useState(true);
+    // const [error, setError] = useState(null);
 
-    useEffect(() => {
-        setLoading(true);
-        setError(null);
+    const [state, dispatch] = useReducer(fetchReducer, initialState);
+
+
+    // useEffect(() => {
+    //      setLoading(true);
+    //      setError(null);
+
+    //     fetchFn()
+    //         .then((result) => {
+    //              setData(result);
+    //              setLoading(false);
+    //         })
+    //         .catch((err) => {
+    //              setError(err.message);
+    //              setLoading(false);
+    //         });
+    // }, [fetchFn]);
+
+
+    useEffect (() => {
+        let ignore = false;
+        dispatch ({type: "FETCH_START"})
 
         fetchFn()
             .then((result) => {
-                setData(result);
-                setLoading(false);
+                if (!ignore) dispatch({type: "FETCH_SUCCESFUL", payload: result});
             })
             .catch((err) => {
-                setError(err.message);
-                setLoading(false);
+                if (!ignore) dispatch({type: "FETCH_ERROR", payload: err})
             });
-    }, [fetchFn]);
 
-    return { data, loading, error };
+        return () => {
+            ignore = true;
+        };
+    }, [fetchFn])
+
+    return state;
 };
