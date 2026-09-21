10079235960e05e7ebb7313b9ac7b2461050c8bb
Fatemeh Chit Saz <fatemeh.chitsaaz7@gmail.com>
2026-09-21 10:17:56 +0330

    used useReducer for actions of user instead of useState

--- src/Users/Users.jsx
+++ src/Users/Users.jsx
@@ -1,22 +1,45 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useReducer } from "react";
 import { useGetData } from "../hooks/getData.js";
 import { getUsers, deleteUser, updateUser } from "../API/API.js";
 import UserSearchFilter from "./UserSearchFilter.jsx";
 import UserTable from "./UserTable.jsx";
 import EditUserModal from "./EditUserModal.jsx";
 
+
+const userReducer = (state, action) => {
+    switch (action.type) {
+        case "SET":
+            return action.payload;
+        case "DELETE":
+            return state.filter((user) => user.id !== action.id);
+        case "UPDATE":
+            return state.map((user) => user.id === action.user.id ? action.user : user);
+        default:
+            return state;
+    }
+}
+
 const Users = () => {
     const { data, loading, error } = useGetData(getUsers);
     const [searchTerm, setSearchTerm] = useState("")
-    const [users, setUsers] = useState([]);
+    // const [users, setUsers] = useState([]);
+    const [users, dispatch] = useReducer(userReducer, []);
     const [editingUser, setEditingUser] = useState(null);
 
-    useEffect(() => {
-        if (data) {
-            setUsers(data);
-        }
-    }, [data])
 
+    // useEffect(() => {
+    //     if (data) {
+    //         setUsers(data);
+    //     }
+    // }, [data])
+
+    useEffect (() => {
+        if (data) {
+            dispatch({type: "SET", payload: data});
+        }
+    }, [data]);
+        
+    
     const filteredUsers = users.filter((user) => {
         const term = searchTerm.toLowerCase();
         return Object.values(user).some((value) =>
@@ -24,33 +47,46 @@ const Users = () => {
         );
     });
 
-    const handleDelete = async (userId) => {
-        try {
-            await deleteUser(userId);
-            setUsers((prevData) => prevData.filter((user) => user.id !== userId));
-        } catch (err) {
-            console.error("خطا در حذف کاربر:", err.message);
-        }
-    }
+    // const handleDelete = async (userId) => {
+    //     try {
+    //         await deleteUser(userId);
+    //         setUsers((prevData) => prevData.filter((user) => user.id !== userId));
+    //     } catch (err) {
+    //         console.error("خطا در حذف کاربر:", err.message);
+    //     }
+    // }
 
-    const handleEdit = (userId) => {
-        const userToEdit = users.find((user) => user.id === userId);
-        setEditingUser(userToEdit);
-    }
+    // const handleEdit = (userId) => {
+    //     const userToEdit = users.find((user) => user.id === userId);
+    //     setEditingUser(userToEdit);
+    // }
+
+    // const handleSaveEdit = async (updatedUser) => {
+    //     try {
+    //         await updateUser(updatedUser.id, updatedUser);
+    //         setUsers((prevUsers) =>
+    //             prevUsers.map((user) =>
+    //                 user.id === updatedUser.id ? updatedUser : user
+    //             )
+    //         );
+    //     } catch (err) {
+    //         console.error("خطا در ویرایش کاربر:", err.message);
+    //     } finally {
+    //         setEditingUser(null);
+    //     }
+    // };
+
 
     const handleSaveEdit = async (updatedUser) => {
         try {
-            await updateUser(updatedUser.id, updatedUser);
-            setUsers((prevUsers) =>
-                prevUsers.map((user) =>
-                    user.id === updatedUser.id ? updatedUser : user
-                )
-            );
+            await updatedUser(updatedUser.id, updatedUser);
+            dispatch({type: "UPDATE", user: updateUser});
         } catch (err) {
-            console.error("خطا در ویرایش کاربر:", err.message);
+            console.error("خطا در ویرایش کاربر", err.message);
         } finally {
             setEditingUser(null);
         }
+
     };
 
     return (
